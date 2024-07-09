3aa1576dadcdab299aefa1c8d002ff11faa51611
albams5 <albams51289@gmail.com>
2024-07-09 11:34:25 +0200

    feat: media queries added. Actual cv and projects img added

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,9 +1,10 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
+import ecommerce from "../../assets/creative-concept-design.png";
 import { ProjectCard } from "../../common/ProjectCard";
-import freshBurger from '../../assets/fresh-burger.png'
-import hipsster from '../../assets/hipsster.png'
-import fitLift from '../../assets/fitlift.png'
+import mobee from '../../assets/mobee.png'
+import vibesync from '../../assets/vibesync.png'
+import calendar from '../../assets/calendar.png'
+import rick from '../../assets/rick.png'
 
 export const Projects = () => {
   return (
@@ -11,31 +12,31 @@ export const Projects = () => {
       <h2 className="sectionTitle">Projects</h2>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={ecommerce}
           link="https://github.com/albams5/react-e-commerce"
           h3="Creative Concept Design"
           p="Furniture E-commerce"
         />
         <ProjectCard
-          src={freshBurger}
+          src={mobee}
           link="https://github.com/albams5/mobee-nextjs"
           h3="Mobee"
           p="Keep track of what you watch"
         />
         <ProjectCard
-          src={hipsster}
+          src={vibesync}
           link="https://github.com/aoteron/Vibesync"
           h3="Vibesync"
           p="Music streaming App"
         />
         <ProjectCard
-          src={hipsster}
+          src={calendar}
           link="https://github.com/albams5/Assembler-TS-Calendar"
           h3="Calendar App"
           p="Interactive calendar application"
         />
         <ProjectCard
-          src={fitLift}
+          src={rick}
           link="https://github.com/albams5/rick-and-morty-individual-project"
           h3="Rick & Morty Frikipedia"
           p="All you need to know about your fav show"
