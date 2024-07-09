70ebceccb1831d6b08b613f80f8909162bfc18d3
albams5 <albams51289@gmail.com>
2024-07-09 10:38:30 +0200

    feat: footer added.

---
+++ src/sections/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from './FooterStyles.module.css'
+export const Footer = () => {
+  return (
+    <section id="footer" className={styles.container}>
+        <p>&copy; 2024 Alba Martín.</p>
+        <br/>
+        <p>All rights reserved.</p>
+    </section>
+  )
+}
