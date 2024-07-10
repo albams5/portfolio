d7b83313184d6a06de0e85df1556cf70ab0bb751
albams5 <albams51289@gmail.com>
2024-07-10 12:16:36 +0200

    feat: projectCard edited to contain deployed project and comments. Project style adapted to this new info.

--- src/common/ProjectCard.tsx
+++ src/common/ProjectCard.tsx
@@ -1,15 +1,21 @@
-type Props ={
-    src: string,
-    link: string,
-    h3: string,
-    p: string
-}
-export const ProjectCard = ({src, link, h3, p}:Props) => {
+type Props = {
+  src: string;
+  link: string;
+  h3: string;
+  p: string;
+  deploy?: string;
+  comment?: string;
+};
+export const ProjectCard = ({ src, link, h3, p, deploy, comment }: Props) => {
   return (
-    <a href={link} target="_blank" >
-    <img className="hover" src={src} alt={`${h3} logo`} />
-    <h3>{h3}</h3>
-    <p>{p}</p>
+    <a href={link} target="_blank">
+      <img className="hover" src={src} alt={`${h3} logo`} />
+      <h3>{h3}</h3>
+      <p>{p}</p>
+      {deploy && (
+          <a href={deploy} target="_blank"><p>Deployed Project</p></a>
+      )}
+      <p style={{ fontSize: '12px' }}>{comment && comment}</p>
     </a>
-  )
-}
+  );
+};
--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -11,35 +11,41 @@ export const Projects = () => {
     <section id="projects" className={styles.container}>
       <h2 className="sectionTitle">Projects</h2>
       <div className={styles.projectsContainer}>
-        <ProjectCard
+      <ProjectCard
           src={ecommerce}
           link="https://github.com/albams5/react-e-commerce"
           h3="Creative Concept Design"
           p="Furniture E-commerce"
+          deploy="https://react-e-commerce-gules.vercel.app/"
+          comment="User: Jane Smith; password: pass1234"
         />
         <ProjectCard
           src={mobee}
           link="https://github.com/albams5/mobee-nextjs"
           h3="Mobee"
           p="Keep track of what you watch"
+          deploy="https://delicate-rabanadas-292188.netlify.app/"
         />
         <ProjectCard
           src={vibesync}
           link="https://github.com/aoteron/Vibesync"
           h3="Vibesync"
           p="Music streaming App"
+          deploy=""
         />
         <ProjectCard
           src={calendar}
           link="https://github.com/albams5/Assembler-TS-Calendar"
           h3="Calendar App"
           p="Interactive calendar application"
+          deploy="https://comfy-melba-4556a9.netlify.app/"
         />
         <ProjectCard
           src={rick}
           link="https://github.com/albams5/rick-and-morty-individual-project"
           h3="Rick & Morty Frikipedia"
           p="All you need to know about your fav show"
+          deploy="https://rick-and-morty-individual-project.vercel.app/"
         />
       </div>
     </section>
